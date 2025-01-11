83974ca65cafd0c2b0c786275d429219ab050c4a
bukkyoudaigaku <bu2321250001@bukkyo-u.ac.jp>
2025-01-11 06:04:23 +0000

    授業

--- twoQ.rb
+++ twoQ.rb
@@ -1,18 +1,42 @@
-question = "今回のスクーリングは始まった日は？"
-options = ["1. 2024/1/11", "2. 2025/1/11", "3. 2025/7/10"]
-answer = "2"  # 正解は "2"
 
-# クイズの表示
-puts question
-options.each { |option| puts option }
+def qa(title, a1, a2, a3, num)
+    puts title
+    puts a1
+    puts a2
+    puts a3
+    input = gets.chomp.to_i
+    if input == num.to_i
+      puts "正解です"
+    else
+      puts "不正解です"
+    end
+  end
+  
+  title = "今回のスクーリングが始まった日は?"
+  a1 = "1 2024/1/11"
+  a2 = "2 2025/1/11"
+  a3 = "3 2025/7/10"
+  num = 2
+  qa(title, a1, a2, a3, num)
+  
 
-# ユーザーからの回答を受け取る
-puts "答えを入力してください (1, 2, 3): "
-user_answer = gets.chomp
 
-# 正誤判定
-if user_answer == answer
-  puts "正解です！"
-else
-  puts "不正解です。"
-end
+  def qa(title, a1, a2, a3, num)
+    puts title
+    puts a1
+    puts a2
+    puts a3
+    input = gets.chomp.to_i
+    if input == num.to_i
+      puts "正解です"
+    else
+      puts "不正解です"
+    end
+  end
+  
+  title = "黄色い果物は?"
+  a1 = "1 オレンジ"
+  a2 = "2 りんご"
+  a3 = "3 バナナ"
+  num = 1
+  qa(title, a1, a2, a3, num)
